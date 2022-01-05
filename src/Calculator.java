0c3f7f5c7a9df6644b7c1dd92f7b21a2a6859c67
Ariel Stitt <arielstitt@Skills-Macbook-Pro-37.local>
2022-01-05 10:14:04 -0500

    added caluclator methods

---
+++ src/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+
+    public int add(int num1, int num2){
+        return num1 + num2;
+    }
+    public int subtract(int num1, int num2){
+        return num1 + num2;
+    }
+    public int mutiply(int num1, int num2){
+        return num1 + num2;
+    }
+    public int divide(int num1, int num2){
+        return num1 + num2;
+    }
+    public int square(int num1){
+        return num1 * num1;
+    }
+}
