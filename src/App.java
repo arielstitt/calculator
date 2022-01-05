c5301ba84e01d4f75456368073417fa8b1d9fbf3
Ariel Stitt <arielstitt@Skills-Macbook-Pro-37.local>
2022-01-05 10:26:35 -0500

    adde vars and prints

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
-    }
+        int r1 = Calculator.add(75, 36);
+        int r2 = Calculator.subtract(84, 26);
+        int r3 = Calculator.mutiply(85, 12);
+        int r4 = Calculator.divide(53, 14);
+        int r5 = Calculator.square(4);
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+        System.out.println(r5);
+        }
 }
