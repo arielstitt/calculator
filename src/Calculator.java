a30a59e890d62d710a5e7c7fe6181cd19f013170
Ariel Stitt <arielstitt@Skills-Macbook-Pro-37.local>
2022-01-05 10:18:30 -0500

    added static to methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,18 +1,18 @@
 public class Calculator {
 
-    public int add(int num1, int num2){
+    public static int add(int num1, int num2){
         return num1 + num2;
     }
-    public int subtract(int num1, int num2){
+    public static int subtract(int num1, int num2){
         return num1 - num2;
     }
-    public int mutiply(int num1, int num2){
+    public static int mutiply(int num1, int num2){
         return num1 * num2;
     }
-    public int divide(int num1, int num2){
+    public static int divide(int num1, int num2){
         return num1 / num2;
     }
-    public int square(int num1){
+    public static int square(int num1){
         return num1 * num1;
     }
 }
